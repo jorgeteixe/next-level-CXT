e80af72754207beeb59d81bd66249397d97ca79f
Jorge Teixeira Crespo <jorgeteixe11@gmail.com>
2022-05-09 19:31:07 +0200

    Update index.js

--- 12-mattermost/20-bots/20-telegram/index.js
+++ 12-mattermost/20-bots/20-telegram/index.js
@@ -4,7 +4,8 @@ const CHAT_ID = process.env.CHAT_ID
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN
 
 exports.handler = async (event) => {
-  for (const { message } of event.Records) {
+  for (const { Sns } of event.Records) {
+    const { Message: message } = Sns
     await axios.get(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${message}`)
   }
 }
