2b44ad9c8a0911853b163f1a98a9666f7b897444
Jorge Teixeira Crespo <jorgeteixe11@gmail.com>
2022-05-09 19:30:52 +0200

    Update index.js

--- 12-mattermost/20-bots/10-mattermost/index.js
+++ 12-mattermost/20-bots/10-mattermost/index.js
@@ -3,7 +3,8 @@ const axios = require('axios')
 const MATTERMOST_WEBHOOK_URL = process.env.MATTERMOST_WEBHOOK_URL
 
 exports.handler = async (event) => {
-  for (const { message } of event.Records) {
+  for (const { Sns } of event.Records) {
+    const { Message: message } = Sns
     await axios.post(MATTERMOST_WEBHOOK_URL, { text: message })
   }
 }
